4df9ed1e783e15e435f61ad257340096fccfd659
Jonathan Berney <jberney1@icloud.com>
2016-10-15 19:08:40 -0700

    removing request log

--- src/app_factory.js
+++ src/app_factory.js
@@ -6,11 +6,6 @@ const RouterFactory = require('./router_factory');
 module.exports = {
     newApp() {
         const app = express();
-        app.use(function (req, res, next) {
-            console.log(`${req.method} ${req.url}`);
-            if (req.body) console.log(req.body);
-            next();
-        });
         app.use(bodyParser.json());
         app.use('/uaa', RouterFactory.newRouter());
         return app;
